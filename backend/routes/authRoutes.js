0c6773d94421ad2e3d0334d86a23011e735e6ce4
Urvi-Ladhani <urviladhani23@gmail.com>
2026-06-19 02:05:34 +0530

    authentication with JWT and login api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    signupUser
+    signupUser,
+    loginUser
 } = require("../controllers/authController");
 
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
 module.exports = router;

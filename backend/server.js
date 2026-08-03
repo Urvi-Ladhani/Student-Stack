1a6e39e8a01a5624f28d98b88d2083137ef763fa
Urvi-Ladhani <urviladhani23@gmail.com>
2026-08-03 22:10:47 +0530

    fix: nuke functions config and setup strict api bridge for vercel

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,9 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (!process.env.VERCEL) {
+if (process.env.NODE_ENV !== 'production') {
    app.listen(PORT, () => {
-      console.log(`Server Running on ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
    });
 }
 
